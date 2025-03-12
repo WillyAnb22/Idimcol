4e44ad50ee3ea2f19ae11890c20ceb6a070f7016
WillyAnb22 <willibl4nc0@gmail.com>
2025-03-12 16:58:57 -0500

    boton de redes

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,19 @@
       </q-header>
       <q-page-container>
         <router-view></router-view>
+        <q-fab
+        v-model="fabRight"
+        vertical-actions-align="right"
+        color="primary"
+        glossy
+        icon="keyboard_arrow_up"
+        direction="up"
+      >
+        <q-fab-action label-position="left" color="primary" @click="onClick" icon="mail" label="Email" />
+        <q-fab-action label-position="left" color="secondary" @click="onClick" icon="alarm" label="Alarm" />
+        <q-fab-action label-position="left" color="orange" @click="onClick" icon="airplay" label="Airplay" />
+        <q-fab-action label-position="left" color="accent" @click="onClick" icon="room" label="Map" />
+      </q-fab>
       </q-page-container>
     </q-layout>
   </div>
@@ -30,7 +43,15 @@
 <script setup>
 import { onMounted } from "vue";
 import { useRouter } from "vue-router";
+import { ref } from 'vue'
 
+const fabLeft = ref(true)
+const fabCenter = ref(true)
+const fabRight = ref(true);
+
+const onClick = () => {
+  console.log('Clicked on a fab action')
+}
 const router = useRouter();
 
 onMounted(() => {

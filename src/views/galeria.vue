62a3aebc7a97e4dc1689fa5c06bf4bc0978432b0
WillyAnb22 <willibl4nc0@gmail.com>
2025-03-25 17:02:44 -0500

    seccion conocenos

--- src/views/galeria.vue
+++ src/views/galeria.vue
@@ -1,63 +1,77 @@
 <template>
-    <div class="q-pa-md">
-      <div class="q-col-gutter-md row">
-        <div class="col-4">
-          <q-img src="fondo.png">
-          </q-img>
-        </div>
-  
-        <div class="col-4">
-          <q-img src="fondo.png">
-          </q-img>
-        </div>
-  
-        <div class="col-4">
-          <q-img src="fondo.png">
-          </q-img>
-        </div>
-  
-        <div class="col-4">
-          <q-img src="fondo.png">
-          </q-img>
-        </div>
-        <div class="col-4">
-            <q-img src="fondo.png">
-            </q-img>
-        </div>
-        <div class="col-4">
-            <q-img src="fondo.png">
-            </q-img>
-        </div>
-        <div class="col-4">
-            <q-img src="fondo.png">
-            </q-img>
-        </div>
-        <div class="col-4">
-            <q-img src="fondo.png">
-            </q-img>
-        </div>
-        <div class="col-4">
-          <q-img src="fondo.png">
-          </q-img>
-        </div>
-        <div class="col-4">
-          <q-img src="fondo.png"></q-img>
-        </div>
+  <div class="q-pa-md">
+    <div class="q-col-gutter-md row">
+      <div class="col-4">
+        <q-img src="fondo.png"></q-img>
+      </div>
+      <div class="col-4">
+        <q-img src="fondo.png"></q-img>
+      </div>
+      <div class="col-4">
+        <q-img src="fondo.png"></q-img>
+      </div>
+      <div class="col-4">
+        <q-img src="fondo.png"></q-img>
+      </div>
+      <div class="col-4">
+        <q-img src="fondo.png"></q-img>
+      </div>
+      <div class="col-4">
+        <q-img src="fondo.png"></q-img>
+      </div>
+      <div class="col-4">
+        <q-img src="fondo.png"></q-img>
+      </div>
+      <div class="col-4">
+        <q-img src="fondo.png"></q-img>
+      </div>
+      <div class="col-4">
+        <q-img src="fondo.png"></q-img>
       </div>
     </div>
-  </template>
+    <div class="q-col-gutter-md row">
+      <p>Sección Videos</p>
+      <div class="col-4">
+        <q-video src="video.mp4"></q-video>
+      </div>
+      <div class="col-4">
+        <video src="video.mp4"></video>
+      </div>
+    </div>
 
-  <style scoped>
-  .q-col-gutter-md row{
-    
-  }
-    .col-4{
-      align-content: center;
-      justify-content: center;
-      width: 30%; /* Ajusta el tamaño de la imagen */
-      transition: transform 0.3s ease-in-out;
-    }
-    .col-4:hover{
-      transform: scale(1.1); /* Se agranda un 10% */
-    }
-  </style>
+  </div>
+</template>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+}
+
+.col-4,
+.video {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30%;
+  /* Ajusta el tamaño de la imagen */
+  transition: transform 0.3s ease-in-out;
+}
+
+.col-4:hover,
+.video:hover {
+  transform: scale(1.1);
+}
+video {
+  width: 70%;
+  height: auto;
+  max-width: 700px; /* Ajusta el tamaño máximo del video */
+}
+
+.full-width {
+  width: 100%;
+}
+
+</style>

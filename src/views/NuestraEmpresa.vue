e82f62042e32dd813ac6f9c364d50c44c1922af1
WillyAnb22 <willibl4nc0@gmail.com>
2025-05-05 13:29:39 -0500

    instalar leaflet para   que el icono del mapa funciones

--- src/views/NuestraEmpresa.vue
+++ src/views/NuestraEmpresa.vue
@@ -112,6 +112,13 @@ onMounted(async () => {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
+  delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL('leaflet/dist/images/marker-icon-2x.png', import.meta.url).href,
+  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
+  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
+});
 
   L.marker([latitude, longitude])
     .addTo(map)
